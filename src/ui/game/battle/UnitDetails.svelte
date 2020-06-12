699f2c9e274a08e57c6e02ff0e1c66e51fb79e87
unknown <etienne@cureous.life>
2020-06-12 21:44:20 +0200

    ui - more readable unit details

--- src/ui/game/battle/UnitDetails.svelte
+++ src/ui/game/battle/UnitDetails.svelte
@@ -5,11 +5,34 @@
 
     export let unit;
 
+    const HP_BAR_SIZE = 100;
+
     $: owner = $State.game.battle.factions[unit.owner];
     $: selectedAbilityName = $State.ui.selected.ability && $State.ui.selected.ability.name;
 
     $: activatedAbilities = unit.abilities.filter(a => a.trigger.type === TriggerType.Activated);
     $: passiveAbilities = unit.abilities.filter(a => a.trigger.type !== TriggerType.Activated || !a.trigger.type);
+
+    let hpBars = [];
+    let energyBars = [];
+    $: {
+        hpBars = [];
+        for (let i = 0; i < unit.hp; i++) {
+            hpBars.push('full');            
+        }
+        for (let i = Math.max(0, unit.hp); i < unit.hpMax; i++) {
+            hpBars.push('empty');            
+        }        
+        energyBars = [];
+        for (let i = 0; i < unit.energy; i++) {
+            energyBars.push('full');            
+        }
+        for (let i = Math.max(0, unit.energy); i < unit.energyMax; i++) {
+            energyBars.push('empty');            
+        }        
+    }
+
+    $: hpColor = unit.owner === 0 ? 'green' : '#ce0e0e';
 
     function onClickPass() {
         sendAction($State.game, ActionType.Pass, {
@@ -37,10 +60,11 @@
         text-transform: capitalize;
     }
     .stats {
-        margin: 10px 0px;
+        margin-bottom: 10px;
     }
     td {
         padding-right: 20px;
+        vertical-align: top;   
     }
     .block-end td {
         padding-bottom: 10px;
@@ -50,12 +74,17 @@
         padding: 10px 20px;
         width: 80%;
         text-align: center;
+        cursor: pointer;
     }
+     .ability-active:hover {
+         background-color: #fafafa;
+     }
     .ability-passive {
         border-top: 1px solid #ccc;
         padding: 10px 0px;
         text-align: center;
-        background-color: #f1f1f1;
+        background-color: #fdfdfd;
+        color: #666;
     }
     .ability-name {
         font-weight: bold;
@@ -66,87 +95,129 @@
         padding-top: 20px;
         text-align: center;
     }
+    svg {
+        margin-top: 3px;
+    }
+    .hp, .energy {
+        stroke: white;
+        stroke-width: 0.5;
+    }
+    .muted {
+        color: #999;
+    }
+    .sml {
+        font-size: 14px !important;
+    }
+    .icon img {
+        width: 20px;
+        height: 20px;
+    }
+    .icon span {
+        font-size: 24px;
+    }
+    .pad-right {
+        padding-right: 10px;
+    }
+    .no-pad {
+        padding: 0;
+    }
 </style>
 
 <div class="unit-card">
     <div class="name">
         { unit.name }
     </div>
-    <div>
-        { owner.name }
-    </div>
     <div class="categories">
-        { unit.category.toLowerCase() } { unit.type && unit.type.toLowerCase() || '' }
+        { unit.category.toLowerCase() } { unit.type && unit.type.toLowerCase() || '' } - { owner.name }
     </div>
     <div class="stats">
         <table>
             <tbody>
-                <tr>
-                    <td>Hit Points</td>
-                    <td>{ unit.hp } / { unit.hpMax }</td>
+                <tr class="block-end">
+                    <td class="icon">
+                        {#if unit.cc.mezz}
+                        <img src="assets/icons/mezz.png" alt="" /> <span class="pad-right">{ unit.cc.mezz }</span>
+                        {/if}
+                        {#if unit.cc.stun}
+                        <img src="assets/icons/stun.png" alt="" /> <span class="pad-right">{ unit.cc.stun }</span>
+                        {/if}
+                        {#if unit.cc.root}
+                        <img src="assets/icons/root.png" alt="" /> <span>{ unit.cc.root }</span>
+                        {/if}
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>                    
+                    <td>                    
+                        <svg height="20" width="{HP_BAR_SIZE}px">
+                        {#each hpBars as hpBar,i}
+                            <rect
+                                class="hp"
+                                x={HP_BAR_SIZE/hpBars.length*i}
+                                y={0}
+                                width="{HP_BAR_SIZE/hpBars.length}px"
+                                height="20px"
+                                style="fill:{hpBar === 'full' ? hpColor : '#ddd'}"
+                            />
+                        {/each}
+                        </svg>               
+                    </td>
+                    <td>{ unit.hp } <span class="muted">/ { unit.hpMax }</span></td>
                 </tr>
                 {#if unit.energy !== undefined}
-                <tr class="block-end">
-                    <td>Energy</td>
-                    <td>{ unit.energy } / { unit.energyMax }</td>
+                <tr class="block-end">                    
+                    <td>                    
+                        <svg height="20" width="{HP_BAR_SIZE}px">
+                        {#each energyBars as energyBar,i}
+                            <rect
+                                class="energy"
+                                x={HP_BAR_SIZE/energyBars.length*i}
+                                y={0}
+                                width="{HP_BAR_SIZE/energyBars.length}px"
+                                height="20px"
+                                style="fill:{energyBar === 'full' ? 'rgb(255, 179, 84)' : '#ddd'}"
+                            />
+                        {/each}
+                        </svg>               
+                    </td>
+                    <td>{ unit.energy } <span class="muted">/ { unit.energyMax }</span></td>
                 </tr>
                 {/if}
                 <tr class="block-end">
-                    <td>Movement</td>
-                    <td>{ unit.movement }</td>
+                    <td class="icon">
+                        <img src="assets/icons/move.png" alt="" /> <span class="pad-right">{ unit.movement }</span>
+                        <img src="assets/icons/armor.png" alt="" /> <span>{ unit.armor }</span>
+                    </td>
+                    <td></td>
                 </tr>
                 <tr class="block-end">
-                    <td>Armor</td>
-                    <td>{ unit.armor }</td>
+                    <td class="icon no-pad" colspan="2">
+                        <img src="assets/icons/sword.png" alt="" />
+                        <span style="color:{ getBuffColor(unit, 'meleeAttack') }">{ unit.meleeAttack } </span>
+                        <span class="sml muted pad-right">ATK</span>
+                        <span style="color:{ getBuffColor(unit, 'meleeDefense') }">{ unit.meleeDefense } </span>
+                        <span class="sml muted pad-right">DEF</span>
+                        <span style="color:{ getBuffColor(unit, 'meleeDamage') }">{ unit.meleeDamage.min }-{ unit.meleeDamage.max } </span>
+                        <span class="sml muted">DMG</span>
+                    </td>
+                    <td></td>
                 </tr>
-                <tr>
-                    <td>Melee Attack</td>
-                    <td style="color:{ getBuffColor(unit, 'meleeAttack') }">{ unit.meleeAttack }</td>
+                <tr class="block-end">
+                    <td class="icon no-pad" colspan="2">
+                        <img src="assets/icons/bow.png" alt="" />
+                        {#if unit.range}
+                        <span style="color:{ getBuffColor(unit, 'rangeAttack') }">{ unit.rangeAttack } </span>
+                        <span class="sml muted pad-right">ATK</span>
+                        {/if}
+                        <span style="color:{ getBuffColor(unit, 'rangeDefense') }">{ unit.rangeDefense } </span>
+                        <span class="sml muted pad-right">DEF</span>
+                        {#if unit.range}
+                        <span style="color:{ getBuffColor(unit, 'rangeDamage') }">{ unit.rangeDamage.min }-{ unit.rangeDamage.max } </span>
+                        <span class="sml muted">DMG</span>
+                        {/if}
+                    </td>
+                    <td></td>
                 </tr>                
-                <tr class="block-end">
-                    <td>Melee Damage</td>
-                    <td>{ unit.meleeDamage.min } to { unit.meleeDamage.max }</td>
-                </tr>
-                <tr>
-                    <td>Melee Defense</td>
-                    <td>{ unit.meleeDefense }</td>
-                </tr>
-                {#if unit.range}
-                <tr>
-                    <td>Range</td>
-                    <td>{ unit.range }</td>
-                </tr>
-                <tr>
-                    <td>Range Attack</td>
-                    <td>{ unit.rangeAttack }</td>
-                </tr>
-                <tr>
-                    <td>Range Damage</td>
-                    <td>{ unit.rangeDamage.min } to { unit.rangeDamage.max }</td>
-                </tr>
-                {/if}
-                <tr class="block-end">
-                    <td>Range Defense</td>
-                    <td>{ unit.rangeDefense }</td>
-                </tr>
-                {#if unit.cc.mezz}
-                <tr>
-                    <td>Mezz</td>
-                    <td>{ unit.cc.mezz } rounds</td>
-                </tr>
-                {/if}
-                {#if unit.cc.stund}
-                <tr>
-                    <td>Stunned</td>
-                    <td>{ unit.cc.stund } rounds</td>
-                </tr>
-                {/if}
-                {#if unit.cc.root}
-                <tr>
-                    <td>Rooted</td>
-                    <td>{ unit.cc.root } rounds</td>
-                </tr>
-                {/if}
             </tbody>
         </table>
     </div>
